05570d54d18c1a11674c633894b79c859babd830
Milou de Koning <miloudekoning@MacBook-Pro-van-Milou.local>
2020-03-26 18:03:52 +0100

    26 maarttt

--- XT2challenge2.js
+++ XT2challenge2.js
@@ -1,121 +1,108 @@
-		/*test
-			var myDate = new Date(),
-				month = myDate.getMonth(), //zero base, dus januari = 0
-				date = myDate.getDate(),
-				day = myDate.getDay(); //zero base, zondag = 0 
+function toonTijd(){
+	var datum = new Date();
+	var uren = datum.getHours(); //0 tot 23
+	var minuten = datum.getMinutes(); //0 tot 59
+	var seconden = datum.getSeconds(); //0 tot 59
 
-			var months = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"];
+	if(uren < 10){
+		uren = "0" + uren;
+	}
 
-			console.log(months[month]);*/
+	if(minuten < 10){
+		minuten = "0" + minuten;
+	}
 
-			
-			function toonTijd(){
-				var datum = new Date();
-				var uren = datum.getHours(); //0 tot 23
-				var minuten = datum.getMinutes(); //0 tot 59
-				var seconden = datum.getSeconds(); //0 tot 59
+	var tijd = uren + ":" + minuten;
 
-				//om op te lossen dat wanneer het bv kwart over 8 's ochtends is er een 0 voor de 8 komt, dus 08:15 ipv 8:15.
-				if(uren < 10){
-					uren = "0" + uren;
-				}
+	document.getElementById("mijnDigitaleKlok").innerHTML = tijd;
 
-				if(minuten < 10){
-					minuten = "0" + minuten;
-				}
+	setInterval(toonTijd, 1000);
+}
 
-				if(seconden < 10){
-					seconden = "0" + seconden;
-				}
+function dagdelen(){
+	var datum = new Date();
+	var uren = datum.getHours();
 
-				var tijd = uren + ":" + minuten /*+ ":" + seconden*/;
+	var container = document.getElementById('container');
 
-				document.getElementById("mijnDigitaleKlok").innerHTML = tijd;
-
-				setInterval(toonTijd, 1000);
-			}
-
-			function dagdelen(){
-				var datum = new Date();
-				var uren = datum.getHours();
-				var container = document.getElementById('container');
-				var berichtOchtend = document.getElementById('berichtOchtend');
-				var berichtMiddag = document.getElementById('berichtMiddag');
-				var berichtAvond = document.getElementById('berichtAvond');
-				var emojiOchtend = document.getElementById('emojiOchtend');
-				var emojiMiddag = document.getElementById('emojiMiddag');
-				var emojiAvond = document.getElementById('emojiAvond');
+	var berichtOchtend = document.getElementById('berichtOchtend');
+	var berichtMiddag = document.getElementById('berichtMiddag');
+	var berichtAvond = document.getElementById('berichtAvond');
+	
+	var emojiOchtend = document.getElementById('emojiOchtend');
+	var emojiMiddag = document.getElementById('emojiMiddag');
+	var emojiAvond = document.getElementById('emojiAvond');
 
 
-				if (uren >= 6 && uren < 12){
-					/*console.log('ochtend')*/
-					/*container.classList.add("ochtend");*/
-					container.className = 'ochtend';
+	if (uren >= 6 && uren < 12){
+		/*console.log('ochtend')*/
+		/*container.classList.add("ochtend");*/
+		container.className = 'ochtend';
 
-					berichtOchtend.className = 'bericht';
-					berichtMiddag.className = 'geenBericht';
-					berichtAvond.className = 'geenBericht';
-					berichtNacht.className = 'geenBericht';
+		berichtOchtend.className = 'bericht';
+		berichtMiddag.className = 'geenBericht';
+		berichtAvond.className = 'geenBericht';
+		berichtNacht.className = 'geenBericht';
 
-					emojiOchtend.className = 'emoji';
-					emojiMiddag.className = 'geenBericht';
-					emojiAvond.className = 'geenBericht';
-					emojiNacht.className = 'geenBericht';
-				}
+		emojiOchtend.className = 'emoji';
+		emojiMiddag.className = 'geenBericht';
+		emojiAvond.className = 'geenBericht';
+		emojiNacht.className = 'geenBericht';
+	}
 
-				else if (uren >=12 && uren < 18) {
-					/*console.log("middag")*/
-					/*container.classList.add("middag");*/
-					container.className = 'middag';
+	else if (uren >=12 && uren < 18) {
+		/*console.log("middag")*/
+		/*container.classList.add("middag");*/
+		container.className = 'middag';
 
-					berichtOchtend.className = 'geenBericht';
-					berichtMiddag.className = 'bericht';
-					berichtAvond.className = 'geenBericht';
-					berichtNacht.className = 'geenBericht';
+		berichtOchtend.className = 'geenBericht';
+		berichtMiddag.className = 'bericht';
+		berichtAvond.className = 'geenBericht';
+		berichtNacht.className = 'geenBericht';
 
-					emojiOchtend.className = 'geenBericht';
-					emojiMiddag.className = 'emoji';
-					emojiAvond.className = 'geenBericht';
-					emojiNacht.className = 'geenBericht';
+		emojiOchtend.className = 'geenBericht';
+		emojiMiddag.className = 'emoji';
+		emojiAvond.className = 'geenBericht';
+		emojiNacht.className = 'geenBericht';
 
-				}
+	}
 
-				else if (uren >=18 && uren < 24){
-					/*console.log("avond")*/
-					/*container.classList.add("avond");*/
-					container.className = 'avond';
+	else if (uren >=18 && uren < 24){
+		/*console.log("avond")*/
+		/*container.classList.add("avond");*/
+		container.className = 'avond';
 
-					berichtOchtend.className = 'geenBericht';
-					berichtMiddag.className = 'geenBericht';
-					berichtAvond.className = 'berichtDonker';
-					berichtNacht.className = 'geenBericht';
+		berichtOchtend.className = 'geenBericht';
+		berichtMiddag.className = 'geenBericht';
+		berichtAvond.className = 'berichtDonker';
+		berichtNacht.className = 'geenBericht';
 
-					emojiOchtend.className = 'geenBericht';
-					emojiMiddag.className = 'geenBericht';
-					emojiAvond.className = 'emoji';
-					emojiNacht.className = 'geenBericht';
+		emojiOchtend.className = 'geenBericht';
+		emojiMiddag.className = 'geenBericht';
+		emojiAvond.className = 'emoji';
+		emojiNacht.className = 'geenBericht';
 
-				}
+	}
 
-				else {
-					container.className = 'nacht';
+	else {
+		container.className = 'nacht';
 
-					berichtOchtend.className = 'geenBericht';
-					berichtMiddag.className = 'geenBericht';
-					berichtAvond.className = 'geenBericht';
-					berichtNacht.className = 'berichtDonker';
+		berichtOchtend.className = 'geenBericht';
+		berichtMiddag.className = 'geenBericht';
+		berichtAvond.className = 'geenBericht';
+		berichtNacht.className = 'berichtDonker';
 
-					emojiOchtend.className = 'geenBericht';
-					emojiMiddag.className = 'geenBericht';
-					emojiAvond.className = 'geenBericht';
-					emojiNacht.className = 'emoji';
+		emojiOchtend.className = 'geenBericht';
+		emojiMiddag.className = 'geenBericht';
+		emojiAvond.className = 'geenBericht';
+		emojiNacht.className = 'emoji';
 
-				}
+	}
 
-				setInterval(dagdelen, 1000);
-			}
+	setInterval(dagdelen, 1000);
+}
 
-			window.onload = function(){
-				dagdelen();
-				toonTijd();
-			}
+window.onload = function(){
+	dagdelen();
+	toonTijd();
+}
